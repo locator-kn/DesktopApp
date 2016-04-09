a8daefa4ada63300d66bd8b6d14a547c12e6917c
Michael Knoch <info@michaelknoch.de>
2016-04-09 16:59:35 +0200

    add dummy servicetest

---
+++ app/service/location.service.spec.ts
@@ -0,0 +1,7 @@
+import {it, describe, expect, beforeEach, inject} from 'angular2/testing';
+
+describe("A suite", function () {
+    it("contains spec with an expectation", function () {
+        expect(true).toBe(true);
+    });
+});
